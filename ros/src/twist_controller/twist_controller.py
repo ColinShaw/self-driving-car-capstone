49736adf56071b98bcdbc3817ed270b7f64c89fd
Colin Shaw <colin.shaw@aya.yale.edu>
2017-08-31 16:23:36 -0500

    Nimish's yaw controller code for steering (much smoother for steering) and some minor updates for clarity

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,41 +1,52 @@
 import rospy
-from   pid import PID 
-
-
-GAS_DENSITY = 2.858
-ONE_MPH     = 0.44704
+from   yaw_controller import YawController
+from   pid            import PID
 
 
 class Controller(object):
 
     def __init__(self):
-        self.steering_pid = PID(1.0, 0.0, 0.0)
-        self.throttle_pid = PID(1.0, 0.0, 0.0)
-        self.brake_pid    = PID(1.0, 0.0, 0.0)
+        wheel_base        = rospy.get_param('~wheel_base',      2.8498)
+        steer_ratio       = rospy.get_param('~steer_ratio',     14.8)
+        max_lat_accel     = rospy.get_param('~max_lat_accel',   3.0)
+        max_steer_angle   = rospy.get_param('~max_steer_angle', 8.0)        
+
         self.last_time    = None
+
+        self.throttle_pid = PID(10.0, 1.0, 0.2)
+        self.brake_pid    = PID(1.0, 0.0, 0.0)
+
+        self.yaw_control  = YawController(wheel_base=wheel_base, 
+                                          steer_ratio=steer_ratio,
+                                          min_speed=0.0, 
+                                          max_lat_accel=max_lat_accel,
+                                          max_steer_angle=max_steer_angle)    
 
 
     def control(self, **kwargs):
-        twist_cmd        = kwargs['twist_cmd']
-        current_velocity = kwargs['current_velocity']
+        tc_l = kwargs['twist_cmd'].twist.linear
+        tc_a = kwargs['twist_cmd'].twist.angular
 
-        tc_l = twist_cmd.twist.linear
-        tc_a = twist_cmd.twist.angular
+        cv_l = kwargs['current_velocity'].twist.linear
+        cv_a = kwargs['current_velocity'].twist.angular
 
-        cv_l = current_velocity.twist.linear
-        cv_a = current_velocity.twist.angular
-            
-        velocity_error = tc_l.x - cv_l.x
-        heading_error  = tc_a.z - cv_a.z
-
+        velocity_error           = tc_l.x - cv_l.x
+        desired_linear_velocity  = tc_l.x
+        desired_angular_velocity = tc_a.z
+        current_linear_velocity  = cv_l.x
+           
         if self.last_time is not None:
             time           = rospy.get_time()
             delta_t        = time - self.last_time
             self.last_time = time
+
             throttle       = self.throttle_pid.update(velocity_error, delta_t)
             brake          = self.brake_pid.update(velocity_error, delta_t)
-            steering       = self.steering_pid.update(heading_error, delta_t)
+            steering       = self.yaw_control.get_steering(desired_linear_velocity, 
+                                                           desired_angular_velocity, 
+                                                           current_linear_velocity)
             return throttle, brake, steering
+
         else:
             self.last_time = rospy.get_time()
             return 0.0, 0.0, 0.0
