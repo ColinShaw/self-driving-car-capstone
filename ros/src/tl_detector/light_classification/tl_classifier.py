a277e69cb84888e54939f07f00db1129b5d3dd7b
James Barfield <jamesbarfield87@gmail.com>
2017-08-31 19:21:47 -0400

    Wrote a simple implementation of color classification in get_classification

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,6 @@
 from styx_msgs.msg import TrafficLight
+import numpy as np
+import cv2
 
 class TLClassifier(object):
     def __init__(self):
@@ -16,4 +18,33 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+
+
+        lower_black = np.array([200,0,0], dtype = "uint8")
+        upper_black = np.array([255,100,100], dtype = "uint8")
+        Red_sum = np.sum(cv2.inRange(image, lower_black, upper_black))
+        lower_black = np.array([0,170,0], dtype = "uint8")
+        upper_black = np.array([150,255,150], dtype = "uint8")
+        Green_sum = np.sum(cv2.inRange(image, lower_black, upper_black))
+        lower_black = np.array([200,200,0], dtype = "uint8")
+        upper_black = np.array([255,255,100], dtype = "uint8")
+        Yellow_sum = np.sum(cv2.inRange(image, lower_black, upper_black))
+
+        if (Red_sum < 500) and (Green_sum < 500) and (Yellow_sum < 500): # 500 is a threshold meaning probabbly not a light
+            return TrafficLight.UNKNOWN
+        elif (Red_sum > Green_sum):
+            if (Red_sum > Yellow_sum):
+                return TrafficLight.RED
+            else:
+                return TrafficLight.YELLOW
+
+        elif (Yellow_sum > Green_sum):
+            if (Yellow_sum > Red_sum):
+                return TrafficLight.YELLOW
+            else:
+                return TrafficLight.RED
+        else:
+            return TrafficLight.GREEN
+
+
+        
