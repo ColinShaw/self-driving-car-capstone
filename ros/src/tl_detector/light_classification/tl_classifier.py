d1644847ac110451d0e6b385cc31372c979bd262
Nicholas Condo <nicholas.condo@gmail.com>
2017-09-10 22:20:21 -0400

    fix: Correct the classifier inference speed issue

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,17 +2,25 @@ from styx_msgs.msg import TrafficLight
 import rospy
 import numpy as np
 import cv2
-import keras
+import tensorflow as tf
+from keras import backend as K
+from keras import layers
 from keras.models import load_model
 from keras.applications.vgg16 import preprocess_input
-import tensorflow as tf
+import time
 
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
+        # https://github.com/tensorflow/tensorflow/issues/6698
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        sess = tf.Session(config=config)
+        K.set_session(sess)
         self.model = load_model('../../../data_science/models/udacity_vgg_fine_tuning_combined.h5')
-        self.graph = tf.get_default_graph()
+        # https://github.com/fchollet/keras/issues/3517
+        self.get_output = K.function([self.model.layers[0].input, K.learning_phase()],
+                                     [self.model.layers[-1].output])
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -25,18 +33,15 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-
-        img = cv2.resize(image, (224,224), interpolation=cv2.INTER_NEAREST)
-        img = np.expand_dims(img, axis=0)
-        img = img.astype('float64')
-        img = preprocess_input(img)
-
-        # Hack found for keras issue https://github.com/fchollet/keras/issues/2397
-        with self.graph.as_default():
-            pred = self.model.predict(img)
+        image = cv2.resize(image, (224,224), interpolation=cv2.INTER_NEAREST)
+        image = np.expand_dims(image, axis=0)
+        image = image.astype(dtype=np.float64, copy=False)
+        image = preprocess_input(image)
+        start_time = time.time()
+        pred = self.get_output([image, 0])[0]
+        rospy.logwarn('---{} seconds---'.format(time.time() - start_time))
 
         # Get the index with max value that corresponds to the predicted state
-        #rospy.logwarn('pred: {}'.format(pred))
         pred = np.argmax(pred)
         rospy.logwarn('argmax: {}'.format(pred))
 
